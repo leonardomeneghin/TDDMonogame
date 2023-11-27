ec8f8f1282d13cd447913ee66e6979b41cd0954b
Leonardo <leonardozenni@gmail.com>
2023-11-27 16:47:00 -0300

    Projeto de jogo Monogame + Projeto de teste NUnit criados

---
+++ TDDMonogame/monogame/monogame.teste/UnitTest1.cs
@@ -0,0 +1,22 @@
+using monogame.Scripts;
+using NUnit.Framework;
+
+
+namespace monogame.teste
+{
+    [TestFixture()]
+    public class UnitTest1
+    {
+        private Tester tester;
+        [SetUp] 
+        public void TestSetUp()
+        {
+            tester = new Tester();
+        }
+        [Test]
+        public void TestCase()
+        {
+            Assert.That(false, Is.EqualTo(tester.isBool()));
+        }
+    }
+}
