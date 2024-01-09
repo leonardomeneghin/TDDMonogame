6691778634d945a92a24110069431240efdda5cb
Leo <leonardozenni@gmail.com>
2024-01-08 21:35:20 -0300

    fix: permitindo que dois players joguem no mesmo board

---
+++ monogame.testes/Handlers/Table/WinStateManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monogame.testes.Handlers.Table
+{
+    [TestFixture()]
+    public class WinStateManagerTests
+    {
+        [Test()]
+        public void When_have_rows_with_0_state_dont_win()
+        {
+
+        }
+    }
+}
