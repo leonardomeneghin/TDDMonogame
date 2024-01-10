3fb88c52f5701883bbcb1e4a302df53df0c4f80e
Leo <leonardozenni@gmail.com>
2024-01-09 21:23:14 -0300

    feat: gerenciador de estado de vitoria ou derrota

--- GameHandlers/Table/WinStateManager.cs
+++ GameHandlers/Table/WinStateManager.cs
@@ -8,6 +8,46 @@ namespace GameHandlers.Table
 {
     public class WinStateManager
     {
+       public int WichPlayerWon(Region[] regions)
+        {
+            //pt-br: N usar variáveis aqui, tipo c1+n => isso cria um b.o. pra outro desenvolvedor ler;
+            //en-US: te vira. zuera... : don't use variables like c1+n here, because its hard to developers read it.
+            var row1 = HasWon(new Region[] { regions[0], regions[1], regions[2] });
+            var row2 = HasWon(new Region[] { regions[3], regions[4], regions[5] });
+            var row3 = HasWon(new Region[] { regions[1], regions[4], regions[7] });
 
+
+            var col1 = HasWon(new Region[] { regions[0], regions[3], regions[6] });
+            var col2 = HasWon(new Region[] { regions[1], regions[4], regions[7] });
+            var col3 = HasWon(new Region[] { regions[2], regions[5], regions[8] });
+
+            var mainDiagonal = HasWon(new Region[] { regions[0], regions[4], regions[8] });
+            var opositeDiagonal = HasWon(new Region[] { regions[2], regions[4], regions[6] });
+
+            return DetermineWinner(new int[] { row1, row2, row3, col1, col2, col3, mainDiagonal , opositeDiagonal });
+        }
+
+       public int HasWon( Region[] regions)
+        {
+            //Human designed
+            //return (regions.Where(x => x.State == 1).All(r => r == regions[0] && r == regions[1] && r == regions[2])
+            //    || regions.Where(x => x.State == -1).All(r => r == regions[0] && r == regions[1] && r == regions[2])) ? regions[0].State : 0;
+
+            //Monter designed
+            return ((regions[0].State == 1 && regions[1].State == 1 && regions[2].State == 1)
+                || regions[1].State == -1 && regions[1].State == -1 && regions[2].State == -1
+                ) ? regions[0].State : 0;
+
+        }
+
+        public int DetermineWinner(int[] fields)
+        {
+            foreach (var field in fields)
+            {
+                if (field != 0)
+                    return field;
+            }
+            return 0;
+        }
     }
 }
--- monogame.testes/Handlers/Table/WinStateManagerTests.cs
+++ monogame.testes/Handlers/Table/WinStateManagerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameHandlers.Table;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,77 @@ namespace monogame.testes.Handlers.Table
     [TestFixture()]
     public class WinStateManagerTests
     {
+        Region[] _regions;
+        WinStateManager _winStateManager;
+        [SetUp]
+        public void SetUp()
+        {
+            _regions  = new Region[9] {
+                new Region(100, 100, 94, 94, null),
+                new Region(206, 100, 88, 94, null),
+                new Region(306, 100, 94, 94, null),
+                new Region(100, 206, 94, 88, null),
+                new Region(206, 206, 88, 88, null),
+                new Region(306, 206, 94, 88, null),
+                new Region(100, 306, 94, 94, null),
+                new Region(206, 306, 88, 94, null),
+                new Region(306, 306, 94, 94, null)
+            };
+            _winStateManager = new WinStateManager();
+        }
         [Test()]
         public void When_have_rows_with_0_state_dont_win()
         {
+            Assert.That(_winStateManager.WichPlayerWon(_regions), Is.EqualTo(0));
+        }
 
+        [Test()]
+        public void When_Row1_Has_State_1_P1_Won()
+        {
+            _regions[0].State = 1;
+            _regions[1].State = 1;
+            _regions[2].State = 1;
+            Assert.That(_winStateManager.WichPlayerWon(_regions), Is.EqualTo(1));
+        }
+        [Test()]
+        public void When_Row_2_Has_State_Negative1_P2_Won()
+        {
+            _regions[3].State = -1;
+            _regions[4].State = -1;
+            _regions[5].State = -1;
+            Assert.That(_winStateManager.WichPlayerWon(_regions), Is.EqualTo(-1));
+        }
+        [Test()]
+        public void When_Col1_Has_State_NegativeOne_P2_Won()
+        {
+            _regions[1].State = -1;
+            _regions[4].State = -1;
+            _regions[7].State = -1;
+            Assert.That(_winStateManager.WichPlayerWon(_regions), Is.EqualTo(-1));
+        }
+        [Test()]
+        public void When_Col1_Doest_Have_All_Internal_State_NegativeOne_NoOneWinsYet()
+        {
+            _regions[1].State = -1;
+            _regions[4].State = 1;
+            _regions[7].State = -1;
+            Assert.That(_winStateManager.WichPlayerWon(_regions), Is.EqualTo(0));
+        }
+        [Test()]
+        public void When_Main_Diagonal_Has_State_1_P1_Won()
+        {
+            _regions[0].State = 1;
+            _regions[4].State = 1;
+            _regions[8].State = 1;
+            Assert.That(_winStateManager.WichPlayerWon(_regions), Is.EqualTo(1));
+        }
+        [Test()]
+        public void When_Main_OpositeDiagonal_Has_State_NegativeOne_P2_Won()
+        {
+            _regions[2].State = -1;
+            _regions[4].State = -1;
+            _regions[6].State = -1;
+            Assert.That(_winStateManager.WichPlayerWon(_regions), Is.EqualTo(-1));
         }
     }
 }
